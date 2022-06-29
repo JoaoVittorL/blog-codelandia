0787964c513579ace9de280f0c4c750e5b181795
joao vittor <vittorsantos234@gmail.com>
2022-06-29 20:16:44 -0300

    chalenge finish

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+ScrollReveal({
+  origin : 'left',
+  distance: '1000px',
+  duration: 800,
+  
+}).reveal(`
+.search-box`)
+
+ScrollReveal({
+    origin : 'bottom',
+    distance: '30px',
+    duration: 700,
+  }).reveal(`
+  .box-1,
+  .box-2,
+  .box-3,
+  .box-4`)
---
+++ md.md
@@ -0,0 +1,11 @@
+## Desafio 1 - Codelândia - Iuri Silva
+
+### ✔ Desafio HTML e CSS
+
+<p>-> HTML semântico</p>
+
+<p>-> SASS</p>
+
+-> [ScrollRevealjs](https://scrollrevealjs.org)
+
+<p>Através do JavaScript.</p>
